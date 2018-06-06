3d578799503cbfa10f68a725c55d07dd33dd9e22
KeithSilcock <silcockk@gmail.com>
2018-06-06 10:51:56 -0700

    created webserver in node

---
+++ client/src/components/header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default (props) => {
+
+
+    return(
+        <div className="container">
+            <div className="col-md-offset-1">
+                <h1 className="hidden-xs hidden-sm col-md-12 page-header"><strong>Student Grade Table</strong>
+                    <small className="col-md-offset-5">Grade Average : <span className="avgGrade"></span></small>
+                </h1>
+                <h3 className="hidden-lg hidden-md col-xs-12"><strong>Student Grade Table</strong>
+                    <small className="col-xs-offset-5">Grade Average : <span className="avgGrade">40</span></small>
+                </h3>
+            </div>
+        </div>
+    )
+}
