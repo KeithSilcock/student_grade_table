24f5e43f68b628e4e15d590ee72d428fd108fb6d
KeithSilcock <silcockk@gmail.com>
2018-06-16 12:00:39 -0700

    added animation to drop-down menu

--- client/src/components/teachers/teacher_portal.js
+++ client/src/components/teachers/teacher_portal.js
@@ -1,13 +1,11 @@
 import React from "react";
 import StudentList from "./student_list";
-import DropDownMenu from "../drop_down_menu";
 
 class TeacherPortal extends React.Component {
   render() {
     return (
       <div>
         <h1>Teacher Portal Header</h1>
-        <DropDownMenu />
         <div className="row">
           <StudentList />
         </div>
