0af6341d1b8a2339ebd5f12a1242fe6b95045473
Keith Silcock <silcockk@gmail.com>
2018-07-23 17:44:50 -0700

    moved some code around for cleanliness

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,20 +1,20 @@
 export default {
   LOGIN: "login",
-  GET_STUDENT_LIST: "get_student_list",
-  GET_STUDENT_ASSIGNMENT_LIST: "get_student_assignment_list",
+  LOGOUT: "logout",
+
   CHANGE_ACTIVE_CLASS: "change_active_class",
-  SET_AVAILABLE_CLASSES: "set_available_classes",
   SET_ACTIVE_STUDENT: "set_active_student",
+  SET_AVAILABLE_CLASSES: "set_available_classes",
+  TOGGLE_SMALL_MODAL: "toggle_small_modal",
+  CLEAR_GOT_STUDENT_NAME: "clear_got_student_name",
+  SET_RECENT_PAGE: "set_recent_page",
+
+  GET_STUDENT_LIST: "get_student_list",
   ADD_NEW_ASSIGNMENT: "add_new_assignment",
   DELETE_ASSIGNMENT: "delete_assignment",
   GET_STUDENT_NAME: "get_student_name",
-  CLEAR_GOT_STUDENT_NAME: "clear_got_student_name",
   ADD_STUDENT_TO_CLASS: "add_student_to_class",
   UPDATE_SCORE: "update_score",
-  SET_RECENT_PAGE: "set_recent_page",
-  LOGOUT: "logout",
-  NAVIGATE_BACK: "navigate_back",
-  TOGGLE_SMALL_MODAL: "toggle_small_modal",
-  //Student data
+
   GET_STUDENT_DATA: "get_student_data"
 };
