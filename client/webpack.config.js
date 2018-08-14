d4bb8dd8a0099bb44324c263ead324bc4cc4a9ed
KeithSilcock <silcockk@gmail.com>
2018-08-14 09:34:40 -0700

    changed webpack config to remove useless comments

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,8 +1,5 @@
 module.exports = {
   plugins: [
-    new CopyWebpackPlugin([
-      // relative path is from src
-      { from: "./src/assets/favicon/favicon.ico" } // <- your path to favicon
-    ])
+    new CopyWebpackPlugin([{ from: "./src/assets/favicon/favicon.ico" }])
   ]
 };
